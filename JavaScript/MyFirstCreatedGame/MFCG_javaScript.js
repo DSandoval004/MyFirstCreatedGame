950af3dcbe72bee8a50c3f19010bc5ef9f9df7e6
Diego Sandoval <dsandoval004@west-mec.org>
2019-02-07 13:04:45 -0700

    Controls
    Added controls, the wasd keys and holding shift to dash.

--- JavaScript/MyFirstCreatedGame/MFCG_javaScript.js
+++ JavaScript/MyFirstCreatedGame/MFCG_javaScript.js
@@ -14,15 +14,26 @@ var gameArena = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArena, 20);
-    },
+        // Keyboard control
+        window.addEventListener('keydown', function (e) {
+            gameArena.keys = (gameArena.keys || []);
+            gameArena.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            gameArena.keys[e.keyCode] = (e.type == "keydown");
+        })
+    }, // Clear arena
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 // sets up the components
 function component(width, height, color, x, y) {
+    this.gamearena = gameArena
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedy = 0;
     this.x = x;
     this.y = y;
     this.update = function () {
@@ -30,9 +41,55 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
 // Updates game arena
 function updateGameArena() {
     gameArena.clear();
+    player.speedX = 0;
+    player.speedY = 0;
+    // Walking controls
+    if (gameArena.keys && gameArena.keys[65]) {
+        player.speedX = -4;
+    }
+    if (gameArena.keys && gameArena.keys[68]) {
+        player.speedX = 4;
+    }
+    if (gameArena.keys && gameArena.keys[87]) {
+        player.speedY = -4;
+    }
+    if (gameArena.keys && gameArena.keys[83]) {
+        player.speedY = 4;
+    }
+    // Running Controls
+    if (gameArena.keys && gameArena.keys[65] && gameArena.keys[16]) {
+        player.speedX = -9;
+    }
+    if (gameArena.keys && gameArena.keys[68] && gameArena.keys[16]) {
+        player.speedX = 9;
+    }
+    if (gameArena.keys && gameArena.keys[87] && gameArena.keys[16]) {
+        player.speedY = -9;
+    }
+    if (gameArena.keys && gameArena.keys[83] && gameArena.keys[16]) {
+        player.speedY = 9;
+    }
+    // Dash Control
+    if (gameArena.keys && gameArena.keys[65] && gameArena.keys[32]) {
+        player.x += -5;
+    }
+    if (gameArena.keys && gameArena.keys[68] && gameArena.keys[32]) {
+        player.x += 5;
+    }
+    if (gameArena.keys && gameArena.keys[87] && gameArena.keys[32]) {
+        player.y += -5;
+    }
+    if (gameArena.keys && gameArena.keys[83] && gameArena.keys[32]) {
+        player.y += 5;
+    }
+    player.newPos();
     player.update();
 }

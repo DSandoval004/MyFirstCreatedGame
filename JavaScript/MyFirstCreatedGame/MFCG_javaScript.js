4601c296452e968669ee99046fa279eaddb4e557
Diego Sandoval <dsandoval004@west-mec.org>
2019-02-07 19:35:26 -0700

    Index and continue on game
    Added Boundaries, made a canvas in the HTML document, set up some styles for the canvas and section.

--- JavaScript/MyFirstCreatedGame/MFCG_javaScript.js
+++ JavaScript/MyFirstCreatedGame/MFCG_javaScript.js
@@ -1,5 +1,12 @@
-// Sets up Variables
+// Player variables
 var player;
+var pScore;
+var pHealth;
+var pStamina;
+var pMana;
+// Boss variables
+var boss;
+var bHealth;
 // Starts Game
 function startGame() {
     player = new component(20, 20, "red", 490, 10);
@@ -7,13 +14,12 @@ function startGame() {
 }
 // Sets up Game Arena
 var gameArena = {
-    canvas: document.createElement("canvas"),
+    canvas: document.getElementById("gameArena"),
     start: function () {
         this.canvas.width = 1000;
         this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArena, 20);
+        this.interval = setInterval(updateGameArena, 16);
         // Keyboard control
         window.addEventListener('keydown', function (e) {
             gameArena.keys = (gameArena.keys || []);
@@ -21,6 +27,9 @@ var gameArena = {
         })
         window.addEventListener('keyup', function (e) {
             gameArena.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keypress', function (e) {
+            gameArena.keys[e.keyCode] = (e.type == "keypress");
         })
     }, // Clear arena
     clear: function () {
@@ -44,6 +53,25 @@ function component(width, height, color, x, y) {
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
+        this.bondries();
+    }
+    this.bondries = function () {
+        var arenaTop = 1;
+        var arenaRight = gameArena.canvas.width - this.width;
+        var arenaBottom = gameArena.canvas.height - this.height;
+        var arenaLeft = 1;
+        if (this.y < arenaTop) {
+            this.y = arenaTop;
+        }
+        if (this.x > arenaRight) {
+            this.x = arenaRight;
+        }
+        if (this.y > arenaBottom) {
+            this.y = arenaBottom;
+        }
+        if (this.x < arenaLeft) {
+            this.x = arenaLeft;
+        }
     }
 }
 // Updates game arena
@@ -53,31 +81,44 @@ function updateGameArena() {
     player.speedY = 0;
     // Walking controls
     if (gameArena.keys && gameArena.keys[65]) {
-        player.speedX = -4;
+        player.speedX = -9;
     }
     if (gameArena.keys && gameArena.keys[68]) {
-        player.speedX = 4;
+        player.speedX = 9;
     }
     if (gameArena.keys && gameArena.keys[87]) {
-        player.speedY = -4;
+        player.speedY = -9;
     }
     if (gameArena.keys && gameArena.keys[83]) {
-        player.speedY = 4;
+        player.speedY = 9;
     }
     // Running Controls
     if (gameArena.keys && gameArena.keys[65] && gameArena.keys[16]) {
-        player.speedX = -9;
+        player.speedX += -9;
     }
     if (gameArena.keys && gameArena.keys[68] && gameArena.keys[16]) {
-        player.speedX = 9;
+        player.speedX += 9;
     }
     if (gameArena.keys && gameArena.keys[87] && gameArena.keys[16]) {
-        player.speedY = -9;
+        player.speedY += -9;
     }
     if (gameArena.keys && gameArena.keys[83] && gameArena.keys[16]) {
-        player.speedY = 9;
+        player.speedY += 9;
     }
-    // Dash Control
+    // Crouch
+    if (gameArena.keys && gameArena.keys[65] && gameArena.keys[86]) {
+        player.speedX = -1;
+    }
+    if (gameArena.keys && gameArena.keys[68] && gameArena.keys[86]) {
+        player.speedX = 1;
+    }
+    if (gameArena.keys && gameArena.keys[87] && gameArena.keys[86]) {
+        player.speedY = -1;
+    }
+    if (gameArena.keys && gameArena.keys[83] && gameArena.keys[86]) {
+        player.speedY = 1;
+    }
+    // Dash Control, jumps over objects
     if (gameArena.keys && gameArena.keys[65] && gameArena.keys[32]) {
         player.x += -5;
     }

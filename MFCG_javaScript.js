372c038f6e80b3755ef121ad29ce9e83f3554e73
Diego Sandoval <dsandoval004@west-mec.org>
2019-02-19 10:45:17 -0700

    Lots of changes
    Did a lot of stuff and forgot to push, so I push

--- MFCG_javaScript.js
+++ MFCG_javaScript.js
@@ -1,9 +1,15 @@
 // Player variables
-var player;
-var pScore;
-var pHealth;
-var pStamina;
-var pMana;
+var player,
+    pScore,
+    pHealth,
+    pMana,
+    pU = false,
+    pR = false,
+    pD = false,
+    pL = false,
+    pS = false,
+    pC = false,
+    pDash = false
 // Boss variables
 var boss;
 var bHealth;
@@ -11,26 +17,75 @@ var bHealth;
 function startGame() {
     player = new component(20, 20, "red", 490, 10);
     gameArena.start();
+    pStamBar.start();
 }
 // Sets up Game Arena
 var gameArena = {
     canvas: document.getElementById("gameArena"),
     start: function () {
-        this.canvas.width = 1000;
-        this.canvas.height = 600;
+        this.canvas.width = 1250;
+        this.canvas.height = 650;
         this.context = this.canvas.getContext("2d");
         this.interval = setInterval(updateGameArena, 16);
         // Keyboard control
-        window.addEventListener('keydown', function (e) {
-            gameArena.keys = (gameArena.keys || []);
-            gameArena.keys[e.keyCode] = (e.type == "keydown");
-        })
-        window.addEventListener('keyup', function (e) {
-            gameArena.keys[e.keyCode] = (e.type == "keydown");
-        })
-        window.addEventListener('keypress', function (e) {
-            gameArena.keys[e.keyCode] = (e.type == "keypress");
-        })
+        document.addEventListener('keydown', push)
+
+        function push(e) {
+            // Movement
+            if (e.keyCode === 87 /* w */ ) {
+                pU = true
+            }
+            if (e.keyCode === 65 /* d */ ) {
+                pL = true
+            }
+            if (e.keyCode === 83 /* s */ ) {
+                pD = true
+            }
+            if (e.keyCode === 68 /* a */ ) {
+                pR = true
+            }
+            // Mvt +
+            if (e.keyCode === 16 /* shift */ ) {
+                pS = true
+            }
+            if (e.keyCode === 86 /* v */ ) {
+                pC = true
+            }
+        }
+        document.addEventListener('keyup', release)
+
+        function release(e) {
+            if (e.keyCode === 87 /* w */ ) {
+                pU = false
+                pStamState = 0
+            }
+            if (e.keyCode === 68 /* d */ ) {
+                pR = false
+                pStamState = 0
+            }
+            if (e.keyCode === 83 /* s */ ) {
+                pD = false
+                pStamState = 0
+            }
+            if (e.keyCode === 65 /* a */ ) {
+                pL = false
+                pStamState = 0
+            }
+            if (e.keyCode === 16 /* shift */ ) {
+                pS = false
+                pStamState = 0
+            }
+            if (e.keyCode === 86 /* v */ ) {
+                pC = false;
+            }
+        }
+        document.addEventListener('keypress', press)
+
+        function press(e) {
+            if (e.keyCode === 32 /* space */ ) {
+                pDash = true;
+            }
+        }
     }, // Clear arena
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -45,21 +100,26 @@ function component(width, height, color, x, y) {
     this.speedy = 0;
     this.x = x;
     this.y = y;
+    // Everytime the game is updated these are updated.
     this.update = function () {
         ctx = gameArena.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    // Updates the postion of the player
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
-        this.bondries();
+        this.boundaries();
     }
-    this.bondries = function () {
-        var arenaTop = 1;
-        var arenaRight = gameArena.canvas.width - this.width;
-        var arenaBottom = gameArena.canvas.height - this.height;
-        var arenaLeft = 1;
+    // Boundaries
+    this.boundaries = function () {
+        // Sets the variable of the boundaries
+        var arenaTop = 30;
+        var arenaRight = gameArena.canvas.width - this.width - 125;
+        var arenaBottom = gameArena.canvas.height - this.height - 10;
+        var arenaLeft = 125;
+        // Checks to make sure the aobjects is in the boundaries, and make sure they stay in.
         if (this.y < arenaTop) {
             this.y = arenaTop;
         }
@@ -74,63 +134,102 @@ function component(width, height, color, x, y) {
         }
     }
 }
+// Stamina Variables
+var pStamState = 0,
+    pStamTot = 600, // ^
+    pStamCur = pStamTot, // Possible expansion for player to change stats
+    pStamReg0 = true,
+    pStamGLR = 1; //^
+// Stamina Calculator/Diplayer
+var pStamBar = {
+    /* 
+        Determine bar percentage: take player current stamina and divide it by total then multiply it by the size of the bar to get pixel size, then apply it to the bar;
+
+        Calculate the player current stamina by first determining what state the player is in, then based off of that take the stamina rate and multiply it by the approite number, and set it to the current stamna
+    */
+    start: function () {
+        // Calculate the bar percentage
+        var pStamBarSize = (pStamCur / pStamTot) * 100;
+        document.getElementById('pStaminaBar').style.height = pStamBarSize + "%";
+        if (pStamCur > pStamTot) {
+            pStamCur = pStamTot;
+            pStamReg0 = true;
+        }
+        if (pStamCur < 0) {
+            pStamCur = 1;
+            pStamReg0 = false;
+        }
+        if (pStamReg0 === false) {
+            document.getElementById('pStaminaBar').style.backgroundColor = 'red';
+        } else if (pStamBarSize > 50) {
+            document.getElementById('pStaminaBar').style.backgroundColor = 'green';
+        } else if (pStamBarSize > 25) {
+            document.getElementById('pStaminaBar').style.backgroundColor = 'yellow';
+        } else if (pStamBarSize > 12.5) {
+            document.getElementById('pStaminaBar').style.backgroundColor = 'orange';
+        } else if (pStamBarSize > 5) {
+            document.getElementById('pStaminaBar').style.backgroundColor = 'red';
+        }
+    },
+    update: function () {
+        pStamBar.start();
+    }
+};
+
 // Updates game arena
 function updateGameArena() {
     gameArena.clear();
     player.speedX = 0;
     player.speedY = 0;
-    // Walking controls
-    if (gameArena.keys && gameArena.keys[65]) {
-        player.speedX = -9;
+    if (pS && pStamReg0) {
+        // Running Controls
+        if (pU) {
+            player.speedY = -18;
+            pStamState = 2;
+        }
+        if (pR) {
+            player.speedX = 18;
+            pStamState = 2;
+        }
+        if (pD) {
+            player.speedY = 18;
+            pStamState = 2;
+        }
+        if (pL) {
+            player.speedX = -18;
+            pStamState = 2;
+        }
+    } else if (pC) {
+        // Crouch
+    } else {
+        // Walking controls
+        if (pU) {
+            player.speedY = -9;
+            pStamState = 1;
+        }
+        if (pR) {
+            player.speedX = 9;
+            pStamState = 1;
+        }
+        if (pD) {
+            player.speedY = 9;
+            pStamState = 1;
+        }
+        if (pL) {
+            player.speedX = -9;
+            pStamState = 1;
+        }
     }
-    if (gameArena.keys && gameArena.keys[68]) {
-        player.speedX = 9;
+    // 0 = still; 1 = walking, 2 = running
+    if (pStamState === 0) {
+        pStamCur += (pStamGLR * 1.5);
+    } else if (pStamState === 1) {
+        pStamCur += pStamGLR;
+    } else if (pStamState === 2) {
+        pStamCur -= (pStamGLR)
     }
-    if (gameArena.keys && gameArena.keys[87]) {
-        player.speedY = -9;
-    }
-    if (gameArena.keys && gameArena.keys[83]) {
-        player.speedY = 9;
-    }
-    // Running Controls
-    if (gameArena.keys && gameArena.keys[65] && gameArena.keys[16]) {
-        player.speedX += -9;
-    }
-    if (gameArena.keys && gameArena.keys[68] && gameArena.keys[16]) {
-        player.speedX += 9;
-    }
-    if (gameArena.keys && gameArena.keys[87] && gameArena.keys[16]) {
-        player.speedY += -9;
-    }
-    if (gameArena.keys && gameArena.keys[83] && gameArena.keys[16]) {
-        player.speedY += 9;
-    }
-    // Crouch
-    if (gameArena.keys && gameArena.keys[65] && gameArena.keys[86]) {
-        player.speedX = -1;
-    }
-    if (gameArena.keys && gameArena.keys[68] && gameArena.keys[86]) {
-        player.speedX = 1;
-    }
-    if (gameArena.keys && gameArena.keys[87] && gameArena.keys[86]) {
-        player.speedY = -1;
-    }
-    if (gameArena.keys && gameArena.keys[83] && gameArena.keys[86]) {
-        player.speedY = 1;
-    }
-    // Dash Control, jumps over objects
-    if (gameArena.keys && gameArena.keys[65] && gameArena.keys[32]) {
-        player.x += -5;
-    }
-    if (gameArena.keys && gameArena.keys[68] && gameArena.keys[32]) {
-        player.x += 5;
-    }
-    if (gameArena.keys && gameArena.keys[87] && gameArena.keys[32]) {
-        player.y += -5;
-    }
-    if (gameArena.keys && gameArena.keys[83] && gameArena.keys[32]) {
-        player.y += 5;
-    }
+    // console.log(pDash);
+    pStamBar.update();
     player.newPos();
     player.update();
-}
+};
